72eda38f109b5afe1c4bf4e61827387ed43426d7
Evander <epierre68@gmail.com>
2024-04-16 00:12:13 -0400

    practice

---
+++ src/components/pageHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function pageHeader() {
+  return (
+    <section>
+      <h2>Our Mission</h2>
+      <p>To deliver outstanding solutions with research, design, and ethics at our core.</p>
+    </section>
+  );
+}
+
+export default pageHeader;
