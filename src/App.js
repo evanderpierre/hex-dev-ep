7de787be18b8e7cbfd257f9624adec100a35c54d
Evander <epierre68@gmail.com>
2024-04-16 17:36:30 -0400

    first version of hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';  // Assuming it's a CSS file
 import CustomBanner from './components/CustomBanner';
 import PageHeader from './components/PageHeader';
+import Pattern from './components/Pattern';
 
 function App() {
   return (
     <div className='navy-bg'>
+      <Pattern />
       <PageHeader />
       <CustomBanner />
     </div>
